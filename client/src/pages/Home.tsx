748d00d3e6f021289e7ad63207d64a158bc8ea86
David Bulauski <davidbulauski@gmail.com>
2026-07-17 12:34:39 -0400

    Equalize spacing between contact info items

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1509,14 +1509,14 @@ Please review and confirm all details are accurate.`;
                 <h3 className="text-2xl font-bold text-gray-900">
                   Contact Information
                 </h3>
-                  <div className="flex gap-4 -mt-2">
+                  <div className="flex gap-4">
                     <Building2 className="w-6 h-6 flex-shrink-0 mt-1" style={{ color: '#03e1ea' }} />
                     <div>
                       <h4 className="font-bold text-gray-900">Novapex Automation</h4>
                     </div>
                   </div>
 
-                  <div className="flex gap-4 mt-2">
+                  <div className="flex gap-4">
                     <Phone className="w-6 h-6 flex-shrink-0 mt-1" style={{ color: '#03e1ea' }} />
                     <div>
                       <h4 className="font-bold text-gray-900">Phone</h4>
