cc1a4914765f7bd8a23b41cdfa2e71fbffae1479
bulauski2005 <davidbulauski@gmail.com>
2026-08-10 12:08:50 -0400

    Update description of expertise in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -805,7 +805,7 @@ Please review and confirm all details are accurate.`;
                       Proven Expertise
                     </h4>
                     <p className="text-gray-600">
-                      Extensive experience in AI software engineering and smart automation development
+                      Extensive experience in software engineering and smart automation solutions
                     </p>
                   </div>
                 </div>
