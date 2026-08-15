4b686882592fc685c2298845296518200abd057c
David Bulauski <davidbulauski@gmail.com>
2026-08-14 20:31:22 -0400

    Replace hero images with hero32.png

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -420,7 +420,7 @@ Please review and confirm all details are accurate.`;
           className="absolute inset-0 opacity-100"
           style={{
             backgroundImage:
-              'url("/Hero24.png")',
+              'url("/hero32.png")',
             backgroundSize: "cover",
             backgroundPosition: "center 85%",
           }}
