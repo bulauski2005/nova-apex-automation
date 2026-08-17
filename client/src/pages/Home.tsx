73ec12aa4b0781541ff8e85d77e2a79f8f9b9e8e
David Bulauski <davidbulauski@gmail.com>
2026-08-17 19:06:05 -0400

    Replace hero image with hero46.png

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -420,7 +420,7 @@ Please review and confirm all details are accurate.`;
           className="absolute inset-0 opacity-100"
           style={{
             backgroundImage:
-              'url("/hero12.png")',
+              'url("/hero46.png")',
             backgroundSize: "cover",
             backgroundPosition: "center 85%",
           }}
