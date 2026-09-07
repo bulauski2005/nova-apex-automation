1f2c8083016b7cf80d93e26f7b69d72d36fda4a5
David Bulauski <davidbulauski@gmail.com>
2026-09-07 12:52:11 -0400

    Lighten hero bottom gradient further

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -428,7 +428,7 @@ Please review and confirm all details are accurate.`;
         />
 
         {/* Bottom teal gradient */}
-        <div className="absolute -bottom-8 inset-x-0 h-48 bg-gradient-to-t from-[#5FE1EE]/25 via-[#5FE1EE]/10 to-transparent" />
+        <div className="absolute -bottom-8 inset-x-0 h-48 bg-gradient-to-t from-[#5FE1EE]/15 via-[#5FE1EE]/5 to-transparent" />
 
         <div className="container mx-auto px-8 sm:px-12 lg:px-20 relative z-10 pt-24 pb-16 md:pt-[10.25rem] md:pb-24">
           <div className="grid lg:grid-cols-2 gap-10 lg:gap-8 items-center">
